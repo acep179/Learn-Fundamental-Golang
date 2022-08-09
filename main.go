04c69320d969e48ce60e0b5a2082827a670f55d4
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-08-09 23:39:03 +0700

    Menambahkan komentar catatan

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"dumbmerch/routes"
 	"fmt"
 	"net/http"
+
 	"github.com/gorilla/mux"
-	"dumbmerch/routes"
 )
 
 func main() {
+
 	r := mux.NewRouter()
 
+	//. Utk pendeklarasian routernya masih sama, yaitu dengan menggunakan mux.NewRouter(). Kemudain, kita panggil RouteInit dari package routes dengan mengetikkan routes.RouteInit sebagaiman adi bawah ini
+
 	routes.RouteInit(r.PathPrefix("/api/v1").Subrouter())
+
+	// . Jika kita lihat ke file routes.go. Maka dapat dilihat bahwa penulisan function RouteInit-nya adalah sebagaimana berikut: func RouteInit(r *mux.Router). Jika dilihat lagi maka terlihat bahwa function RouteInit membutuhkan parameter. Adapun parameternya adalah mux.Router. Maka dari itu, dalam pemanggilan function RouteInit di sini pun diberikan parameternya. yaitu, segala sesuatu yg berada di dlm tanda kurung RouteInit().
+
+	//ctt Jika dijabarkan tanpa memisahkan file dan folder:
+	// mux.NewRouter().PathPrefix("/api/v1").Subrouter().HandleFunc("/todos", handlers.FindTodos).Methods("GET")
+	// mux.NewRouter().PathPrefix("/api/v1").Subrouter().HandleFunc("/todo", handlers.GetTodo).Methods("GET")
+
+	//todo Untuk pemanggilan function dari package lain, kita harus mengetikkan nama package-nya terlebih dahulu barulah nama function-nya
+
+	//todo Perhatikan penamaan package dan function, pastikan sudah sama / sudah sesuai dengan sumbernya!
 
 	fmt.Println("server running localhost:5000")
 	http.ListenAndServe("localhost:5000", r)
 }
-
--- routes/routes.go
+++ routes/routes.go
@@ -10,3 +10,11 @@ import (
 func RouteInit(r *mux.Router) {
 	TodoRoutes(r)
 }
+
+//. Route init adalah utk menginisialisasi route-nya. Route init itu diambil dari mux.Router. Adapun mux.Router akan menerima nilai routing yg kita kirimkan dari main.go (PathPrefix). Sehingga, seluruh routing kita akan kita grup ke dalam /api/v1 diikuti dengan /todos, /users, /products, dll.
+
+//. Penulisan function RouteInit harus diawali dengan huruf kapital.
+
+//. Karena Function RouteInit akan dimasukkan atau digunakan ke dalam file main.go. Maka, kita harus memastikan bahwa func RouteInit merupakan sebuah func global. Adapun cara memastikannya adalah, kita harus menuliskan kata awalnya dengan menggunakan huruf kapital: RouteInit, bukan huruf kecil: routeInit
+
+//. Jika RouteInit diawali dengan huruf kecil, maka golang akan menganggap bahwa routeInit adalah sebuah function local. Sehingga, routeInit tdk akan dapat digunakakn di package lain.
